4be63e977a8c5e70da6a41198d1d0c14b2ce4995
Lucjan Krzywdziński <lucjan.krzywdzinski@icloud.com>
2024-08-30 17:08:24 +0200

    Repaired script

--- scripts/packages-list.sh
+++ scripts/packages-list.sh
@@ -1,2 +1,6 @@
-(comm -23 <(pacman -Qqen | sort) <(sort ~/dotfiles/other-data/pkglist.md)) >> ~/dotfiles/other-data/pkglist.md
-(comm -23 <(pacman -Qqem | sort) <(sort ~/dotfiles/other-data/aurlist.md)) >> ~/dotfiles/other-data/aurlist.md
+#!/usr/bin/env bash
+
+homeDir=/home/shadow
+
+(comm -23 <(pacman -Qqen | sort) <(sort $homeDir/dotfiles/other-data/pkglist.md)) >> $homeDir/dotfiles/other-data/pkglist.md
+(comm -23 <(pacman -Qqem | sort) <(sort $homeDir/dotfiles/other-data/aurlist.md)) >> $homeDir/dotfiles/other-data/aurlist.md
