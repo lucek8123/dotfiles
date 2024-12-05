11363e03437a6fa19c5b414b4ab03b48760d9088
Lucjan Krzywdziński <lucjan.krzywdzinski@icloud.com>
2024-12-05 23:13:54 +0100

    Changes to nvidia selection script

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -6,24 +6,29 @@ gpu_vd="$(cat /sys/bus/pci/devices/$gpu/vendor) $(cat /sys/bus/pci/devices/$gpu/
 aud_vd="$(cat /sys/bus/pci/devices/$aud/vendor) $(cat /sys/bus/pci/devices/$aud/device)"
 
 function bind_vfio {
-  echo "$gpu" > "/sys/bus/pci/devices/$gpu/driver/unbind"
-  echo "$aud" > "/sys/bus/pci/devices/$aud/driver/unbind"
-  echo "$gpu_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
-  echo "$aud_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
+  lsof /dev/nvidia* | awk '{print $2}' | xargs -I {} kill {}
+  sleep 0.1
+  # rmmod nvidia_uvm nvidia_modeset nvidia nvidia-drm
+  echo "$gpu" | tee "/sys/bus/pci/devices/$gpu/driver/unbind"
+  echo "$aud" | tee "/sys/bus/pci/devices/$aud/driver/unbind"
+  echo "$gpu_vd" | tee /sys/bus/pci/drivers/vfio-pci/new_id
+  echo "$aud_vd" | tee /sys/bus/pci/drivers/vfio-pci/new_id
 }
  
 function unbind_vfio {
-  echo "$gpu_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
-  echo "$aud_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
-  echo 1 > "/sys/bus/pci/devices/$gpu/remove"
-  echo 1 > "/sys/bus/pci/devices/$aud/remove"
-  echo 1 > "/sys/bus/pci/rescan"
+  echo "$gpu_vd" | tee "/sys/bus/pci/drivers/vfio-pci/remove_id"
+  echo "$aud_vd" | tee "/sys/bus/pci/drivers/vfio-pci/remove_id"
+  echo 1 | tee "/sys/bus/pci/devices/$gpu/remove"
+  echo 1 | tee "/sys/bus/pci/devices/$aud/remove"
+  echo 1 | tee "/sys/bus/pci/rescan"
 }
 
+# bind_vfio
+# echo "0000:01:00.0" | tee "/sys/bus/pci/devices/0000:01:00.0/driver/unbind"
 if [ -f ~/.cache/nvidia-status ]; then 
-    if grep -q "yes" ~/.cache/nvidia-status; then 
-        bind_vfio
-    else 
+    if grep -q "enabled" ~/.cache/nvidia-status; then 
         unbind_vfio
+    else 
+        bind_vfio
     fi
 fi
