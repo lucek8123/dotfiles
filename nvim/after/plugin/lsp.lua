9027bccf3376855a41f4431dd195a39de654c179
Lucjan Krzywdziński <lucjan.krzywdzinski@icloud.com>
2025-06-08 22:25:55 +0200

    Added html neovim configuration

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = {buffer = event.buf}
 
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({border = 'rounded'}) end, opts)
     vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
@@ -35,6 +35,7 @@ require('mason').setup({
     }
 })
 
+-- Mason lsp config
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
@@ -50,6 +51,7 @@ require('mason-lspconfig').setup({
   },
 })
 
+-- Mason install debugger adapters 
 require("mason-nvim-dap").setup({
     ensure_installed = { "codelldb" }
 })
@@ -77,6 +79,9 @@ cmp.setup({
     end,
   },
 })
+
+-- Setup autoclose 
+require("autoclose").setup()
 
 -- Snippets
 local ls = require('luasnip')
